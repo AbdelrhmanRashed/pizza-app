376f91f1e2b3a87416c02cde2ef3bfa80eaea9e1
AbdelrhmanRashed <abdoe4567@gmail.com>
2025-11-15 23:33:55 +0200

    second-commit

---
+++ src/features/user/CreateUser.tsx
@@ -0,0 +1,31 @@
+import { useState, FormEvent, ChangeEvent } from 'react';
+
+function CreateUser() {
+  const [username, setUsername] = useState<string>('');
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>👋 Welcome! Please start by telling us your name:</p>
+
+      <input
+        type="text"
+        placeholder="Your full name"
+        value={username}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+      />
+
+      {username !== '' && (
+        <div>
+          <button>Start ordering</button>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default CreateUser;
+
---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,41 @@
+interface Position {
+  latitude: number;
+  longitude: number;
+}
+
+interface AddressData {
+  locality?: string;
+  city?: string;
+  postcode?: string;
+  countryName?: string;
+}
+
+import { getAddress } from './apiGeocoding';
+
+interface FetchAddressResult {
+  position: Position;
+  address: string;
+}
+
+function getPosition(): Promise<GeolocationPosition> {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+async function fetchAddress(): Promise<FetchAddressResult> {
+  // 1) We get the user's geolocation position
+  const positionObj = await getPosition();
+  const position: Position = {
+    latitude: positionObj.coords.latitude,
+    longitude: positionObj.coords.longitude,
+  };
+
+  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+  const addressObj = await getAddress(position);
+  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+  // 3) Then we return an object with the data that we are interested in
+  return { position, address };
+}
+
