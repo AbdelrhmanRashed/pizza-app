0c6c75109e7b26814d1c3678e6fd93509073c15b
AbdelrhmanRashed <abdoe4567@gmail.com>
2025-11-15 23:58:42 +0200

    second-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import { useState, type JSX } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
 
-function App(): JSX.Element {
-  const [id, setID] = useState<number>(0);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
 
-  return <div>Hello vite!</div>;
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
